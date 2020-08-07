0ba4ea6fdb53a52122030038e7b127faf398e039
Hasan-new <mdhasan.yahoo@gmail.com>
2020-08-08 01:03:38 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// 0
+// ""
+// const name = "0";
+// undefined
+// null
+// Nan
+// upper sob condition false.
+let name;
+console.log(name);
+
+if (name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
