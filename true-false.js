69ab7049f11a87e2c13f94587ecc519529e0cba7
Hasan-new <mdhasan.yahoo@gmail.com>
2020-08-08 01:04:18 +0600

    true-false

--- true-false.js
+++ true-false.js
@@ -2,9 +2,14 @@
 // ""
 // const name = "0";
 // undefined
+// false
 // null
 // Nan
 // upper sob condition false.
+
+// '0', ' ', [], {}
+// false jodi string er moddhe ani tobe true hobe 
+
 let name;
 console.log(name);
 
