6d1d76ada11f283b9b435c5f24b3a6e05795eb14
Hasan-new <mdhasan.yahoo@gmail.com>
2020-08-10 00:32:40 +0600

    commited

---
+++ array-split.js
@@ -0,0 +1,12 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// const part = nums.slice(2, 5);
+
+const removed = nums.splice(2,3, 77);
+// console.log(removed);
+// remove hobe array position 3,4 baki ta 
+// rasult hesebe asbe .
+// console.log(nums); 
+// origilal array 
+
+const together = nums.join("ami")
+console.log(together);
